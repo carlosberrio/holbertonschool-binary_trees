3ce01f4d6875d67bb47457643c9df07e01794d4c
Carlos Berrio <4634@holbertonschool.com>
2022-07-25 15:42:38 -0500

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
     if (tree == NULL)
         return (0);
-
+    if (tree->left == NULL && tree->right == NULL)
+        return (0);
     left = binary_tree_height(tree->left);
     right = binary_tree_height(tree->right);
 
